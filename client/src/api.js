6117c24ca719da8738b39139d30b411b90c105f9
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-02 01:06:21 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,7 @@
 // client/src/api.js
 import axios from "axios";
 
-// Backend base URL from .env
-const base = (process.env.REACT_APP_API_URL || "http://localhost:5000") + "/api";
-
-const api = axios.create({ baseURL: base });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL});
 
 // Interceptor to attach token automatically
 api.interceptors.request.use((config) => {
